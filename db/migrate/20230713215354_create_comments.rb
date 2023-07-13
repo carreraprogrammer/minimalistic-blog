9ba0bd8cbae85936bd77e5a7b11da1c836c50c81
Daniel <carreraprogrammer@gmail.com>
2023-07-13 17:27:00 -0500

    Correct index bug and actualize tables

--- db/migrate/20230713215354_create_comments.rb
+++ db/migrate/20230713215354_create_comments.rb
@@ -7,8 +7,5 @@ class CreateComments < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
-    
-    add_index :comments, :author_id
-    add_index :comments, :post_id
   end
 end
--- db/migrate/20230713215710_create_likes.rb
+++ db/migrate/20230713215710_create_likes.rb
@@ -6,8 +6,5 @@ class CreateLikes < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
-
-    add_index :likes, :author_id
-    add_index :likes, :post_id
   end
 end
