94bc8aaaa2369a9c32c30f5c964fe90e13eb9c2b
Daniel <carreraprogrammer@gmail.com>
2023-07-15 18:50:32 -0500

    Add validations to post counter in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   validates :name, presence: true
+  validates :post_counter, numericality: {greater_than_or_equal_to: 0}
+  validates :quantity, numericality: { only_integer: true }
 
   def recent_posts
     posts.includes(:author).order(created_at: :desc).limit(3)
