5b781d305f00c1fbcb486f5e8213298b7e6674b9
Daniel <carreraprogrammer@gmail.com>
2023-07-14 21:16:34 -0500

    Remove duplicate

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,6 @@
 class Like < ApplicationRecord
   has_many :posts, dependent: :destroy, foreign_key: :author_id
   has_many :likes, dependent: :destroy
-  has_many :likes, dependent: :destroy
   after_save :update_comments_counter
   after_destroy :decrease_comments_counter
 
