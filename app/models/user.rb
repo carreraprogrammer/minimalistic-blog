d36cb4aa025b9cba47559209505e8b58054254f1
Daniel <carreraprogrammer@gmail.com>
2023-07-14 19:36:27 -0500

    Create recent_post method for user model'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy, foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  def recent_posts
+    posts.includes(:author).order(created_at: :desc).limit(3)
+  end
 end
