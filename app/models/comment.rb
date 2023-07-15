f4499daba20cabaa55344bb0c4109f34d1f4b3de
Daniel <carreraprogrammer@gmail.com>
2023-07-14 20:16:05 -0500

    Fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
   after_save :update_comments_counter
-  after_destroy :decrease_comments_counter 
+  after_destroy :decrease_comments_counter
 
   def update_comments_counter
     post.increment!(:comments_counter)
