4a6e49427f679225902cfbda5c7d5d079b396b99
Daniel <carreraprogrammer@gmail.com>
2023-07-14 20:09:58 -0500

    Fix comment model bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
   after_save :update_comments_counter
-  after_destroy :decrease_comments_counter
+  after_destroy :decrease_comments_counter 
 
   def update_comments_counter
     post.increment!(:comments_counter)
@@ -10,4 +10,5 @@ class Comment < ApplicationRecord
 
   def decrease_comments_counter
     post.decrement!(:comments_counter)
+  end
 end
