43d8d442068ffdbc10ab8b3f5b81c43dfcb66ecf
Daniel <carreraprogrammer@gmail.com>
2023-07-15 20:54:06 -0500

    Add spec for Comment method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post
   after_save :update_comments_counter
   after_destroy :decrease_comments_counter
+  validates :text, presence: true, length: { in: 2..100 }
 
   def update_comments_counter
     post.increment!(:comments_counter)
---
+++ spec/comment_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  let(:user) { User.new(name: 'Daniel', photo: 'photo.png', bio: 'Lorem ipsum') }
+  let(:post) { Post.new(author: user, title: 'Hello rails', text: 'Rails is great') }
+
+  subject { Comment.new(post: post, author: user, text: 'Hello!') }
+
+  before { subject.save }
+
+  describe 'validations' do
+    it { should validate_presence_of(:text) }
+    it { should validate_length_of(:text).is_at_most(100) }
+  end
+
+  it 'belongs to an author' do
+    expect(subject.author).to eql user
+  end
+
+  it 'belongs to a post' do
+    expect(subject.post).to eql post
+  end
+
+  it 'increments the comments_counter of the post' do
+    expect(post.comments_counter).to eq(1)
+  end
+
+  it 'decrements the comments_counter of the post' do
+    subject.decrease_comments_counter
+    expect(post.comments_counter).to eq(0)
+  end
+end
