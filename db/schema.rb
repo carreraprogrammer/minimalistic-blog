8c062acc955d8694d32e92b129e41071701aefa9
Daniel <carreraprogrammer@gmail.com>
2023-07-15 19:11:24 -0500

    Make post counter column as 0 by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_15_001513) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_16_000853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_15_001513) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "post_counter"
+    t.integer "post_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
