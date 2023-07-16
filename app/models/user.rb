a03a9a16509cb6f1bb3b15d05756673d9e80d5b3
Daniel <carreraprogrammer@gmail.com>
2023-07-15 19:02:55 -0500

    Correct bug on post_counter validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes
   validates :name, presence: true
   validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :quantity, numericality: { only_integer: true }
+  validates :post_counter, numericality: { only_integer: true }
 
   def recent_posts
     posts.includes(:author).order(created_at: :desc).limit(3)
