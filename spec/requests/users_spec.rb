8860afbe3802c1666e8435a04e43117debc13073
Daniel <carreraprogrammer@gmail.com>
2023-07-20 16:17:05 -0500

    Test show method in User controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
+  subject {User.new(name: 'Daniel', photo: 'photo.png', bio: 'Lorem ipsum', post_counter: 3)}
+  before { subject.save }
 
-    subject {User.new(name: 'Daniel', photo: 'photo.png', bio: 'Lorem ipsum', post_counter: 3)}
+  describe 'GET /index' do
 
     it 'returns http success' do
       get users_path
@@ -17,8 +18,25 @@ RSpec.describe 'Users', type: :request do
     end
 
     it "includes the correct placeholder text in the response body" do
-      subject.save
       get users_path
+      expect(response.body).to include(subject.name)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get user_path(subject.id)
+      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the correct template" do
+      get user_path(subject.id)
+      expect(response).to render_template(:show)
+    end
+
+    it "includes the correct placeholder text in the response body" do
+      get user_path(subject.id)
       expect(response.body).to include(subject.name)
     end
   end
