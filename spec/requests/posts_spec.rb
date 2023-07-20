34755bbd60c95eea320949466216fb5f30a0be98
Daniel <carreraprogrammer@gmail.com>
2023-07-20 16:33:06 -0500

    Fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,43 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-
   subject { User.new(name: 'Daniel', photo: 'photo.png', bio: 'Lorem ipsum', post_counter: 3) }
 
-  let(:post_one) {Post.new(author: subject, title: 'Hello rails', text: 'Rails is great', comments_counter: 0, likes_counter: 0)}
+  let(:post_one) do
+    Post.new(author: subject, title: 'Hello rails', text: 'Rails is great', comments_counter: 0, likes_counter: 0)
+  end
 
   before { subject.save }
   before { post_one.save }
-  before { get user_posts_path(subject.id)}
+  before { get user_posts_path(subject.id) }
 
   describe 'GET /index' do
-    it "returns http success" do
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
     end
 
-    it "renders the correct template" do
+    it 'renders the correct template' do
       expect(response).to render_template(:index)
     end
 
-    it "includes the correct placeholder text in the response body" do
+    it 'includes the correct placeholder text in the response body' do
       expect(response.body).to include(post_one.title)
     end
   end
 
   describe 'GET /show' do
-    it "returns http success" do
+    it 'returns http success' do
       get user_post_path(subject.id, post_one.id)
       expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
     end
 
-    it "renders the correct template" do
+    it 'renders the correct template' do
       get user_post_path(subject.id, post_one.id)
       expect(response).to render_template(:show)
     end
 
-    it "includes the correct placeholder text in the response body" do
+    it 'includes the correct placeholder text in the response body' do
       get user_post_path(subject.id, post_one.id)
       expect(response.body).to include(post_one.title)
     end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  subject {User.new(name: 'Daniel', photo: 'photo.png', bio: 'Lorem ipsum', post_counter: 3)}
+  subject { User.new(name: 'Daniel', photo: 'photo.png', bio: 'Lorem ipsum', post_counter: 3) }
   before { subject.save }
 
   describe 'GET /index' do
-
     it 'returns http success' do
       get users_path
       expect(response).to have_http_status(:success)
       expect(response.status).to eq(200)
     end
 
-    it "renders the correct template" do
+    it 'renders the correct template' do
       get users_path
       expect(response).to render_template(:index)
     end
 
-    it "includes the correct placeholder text in the response body" do
+    it 'includes the correct placeholder text in the response body' do
       get users_path
       expect(response.body).to include(subject.name)
     end
@@ -30,12 +29,12 @@ RSpec.describe 'Users', type: :request do
       expect(response.status).to eq(200)
     end
 
-    it "renders the correct template" do
+    it 'renders the correct template' do
       get user_path(subject.id)
       expect(response).to render_template(:show)
     end
 
-    it "includes the correct placeholder text in the response body" do
+    it 'includes the correct placeholder text in the response body' do
       get user_path(subject.id)
       expect(response.body).to include(subject.name)
     end
