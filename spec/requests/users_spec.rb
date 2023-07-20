1c97257079f08e73c59938ec0329c4d62f0a1018
Daniel <carreraprogrammer@gmail.com>
2023-07-20 16:13:47 -0500

    Add test for index method in User controller"

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+
+    subject {User.new(name: 'Daniel', photo: 'photo.png', bio: 'Lorem ipsum', post_counter: 3)}
+
+    it 'returns http success' do
+      get users_path
+      expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the correct template" do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it "includes the correct placeholder text in the response body" do
+      subject.save
+      get users_path
+      expect(response.body).to include(subject.name)
+    end
   end
 end
