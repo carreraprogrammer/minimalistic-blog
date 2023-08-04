081e46c1bc9e598e8d89143f8e655687f08e4130
Daniel Carrera <carreraprogrammer@gmail.com>
2023-08-04 16:35:27 -0500

    add authentication for posts_controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::PostsController < Api::V1::ApplicationController
+  before_action :authenticate_user
   def index
     posts = User.find(params['user_id']).posts
     render json: posts
